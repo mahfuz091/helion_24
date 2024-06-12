29f5cc4babcb0e4b349bcc307c149386dc3e0383
MohammadMasum <masummj987@gmail.com>
2024-06-13 01:05:05 +0600

    Final update with slider section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Banner from "@/app/components/Banner/Banner";
+import Features from "@/app/components/Features/Features";
+import MissionVission from "@/app/components/MissionVission/MissionVission";
 export default function homePage() {
   return (
     <div>
       <Banner/>
+      <Features/>
+      <MissionVission/>
     </div>
   )
 }
--- app/solution/page.js
+++ app/solution/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CommonBanner from "../components/CommonBanner/CommonBanner";
 import YieldAutomation from "../components/YieldAutomation/YieldAutomation";
+import InnovativeStrategies from "../components/InnovativeStrategies/InnovativeStrategies";
 import FuturePlans from "../components/FuturePlans/FuturePlans";
 import OurVision from "../components/OurVision/OurVision";
 import Generation from "../components/Generation/Generation";
@@ -10,6 +11,7 @@ const SolutionPage = () => {
     <div>
       <CommonBanner title='solution' />
       <YieldAutomation />
+      <InnovativeStrategies />
       <FuturePlans />
       <Generation />
       <OurVision />
